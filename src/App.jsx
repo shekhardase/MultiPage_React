3dcc7ed53a6ade7f9c0bec23657985360caeb1cf
Shekhar <115777486+shekhardase@users.noreply.github.com>
2025-02-14 00:38:51 +0530

    Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,44 @@
 import './App.css'
 import Navbar from './components/Navbar'
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Route, Navigate } from 'react-router-dom'
 import { About } from './components/About'
 import { Contact } from './components/Contact'
 import { Home } from './components/Home'
 
-
 function App() {
-
   const router = createBrowserRouter([
     {
+      path: '/',
+      element: <Navigate to="/home" />, // Redirects to Home page
+    },
+    {
       path: 'home',
-      element: <><Navbar /> <Home /></>
+      element: <>
+        <Navbar /> 
+        <Home />
+      </>
     },
     {
       path: 'about',
-      element: <>    <Navbar /> <About />
+      element: <>
+        <Navbar /> 
+        <About />
       </>
-
     },
     {
       path: 'contact',
-      element:
-        <> <Navbar /> <Contact /> </>
+      element: <>
+        <Navbar /> 
+        <Contact />
+      </>
     },
-  ])
+    {
+      path: '*', // Handles unknown routes (404)
+      element: <h1 className="text-center mt-10 text-3xl text-red-500">404 Not Found</h1>,
+    }
+  ]);
 
-  return (
-
-    <RouterProvider router={router} />
-  
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
-
+export default App;
